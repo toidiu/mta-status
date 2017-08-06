5e2f9ea73b0b7d36788976430a61be423a6e55c8
toidiu <toidiu@protonmail.com>
2017-08-06 18:17:04 -0400

    using some cpu pools and handler and core to do some async..

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,18 @@
 //#![deny(warnings)]
+#![allow(unused)]
 extern crate futures;
 extern crate hyper;
 extern crate mta_status;
 extern crate serde_json;
+extern crate tokio_core;
+extern crate futures_cpupool;
+
+use futures::Future;
+use futures_cpupool::CpuPool;
+use tokio_core::reactor::Core;
+use futures::BoxFuture;
+use hyper::Error;
+use futures::future::FutureResult;
 
 use hyper::server::{Server, Request, Response};
 use hyper::{Method, StatusCode};
@@ -10,80 +20,110 @@ use hyper::header::ContentLength;
 use hyper::server::{Http, Service};
 use std::time::Duration;
 use std::thread;
-use futures::future::{Either, Map};
-use futures::stream::Concat2;
-use std::ascii::AsciiExt;
-use futures::Stream;
-use futures::stream::Map;
-use hyper::Chunk;
+
+use futures::future::join_all;
 
 fn main() {
+    //    const PHRASE: &'static str = "Hello, World!";
 
 
-    const PHRASE: &'static str = "Hello, World!";
+    let pool = CpuPool::new_num_cpus();
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
 
-    struct HelloWorld;
-    impl Service for HelloWorld {
-        // boilerplate hooking up hyper's server types
-        type Request = Request;
-        type Response = Response;
-        type Error = hyper::Error;
-        // The future representing the eventual Response your call will
-        // resolve to. This can change to whatever Future you need.
-        type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+    let fut = pool.spawn_fn(|| {
+        let prime = true;
 
-        fn call(&self, _req: Request) -> Self::Future {
-            thread::sleep(Duration::from_secs(5));
-
-            futures::future::ok(
-                Response::new()
-                    //.with_header(ContentLength(PHRASE.len() as u64))
-                    //.with_body(PHRASE)
-                    .with_header(ContentLength(prime.to_string().len() as u64))
-                    .with_body(prime.to_string())
-            )
-        }
-    }
+//        thread::sleep(Duration::from_secs(5));
+        println!("hi from the future");
 
 
-    struct Echo;
-    impl Service for Echo {
-    type Future = Either<
-        FutureResult<Self::Response, Self::Error>,
-        Map<Concat2<Body>, fn(Chunk) -> Self::Response>
-    >;
-    // back to default Response
-        type Response = Response;
+        let res: Result<bool, ()> = Ok(prime);
+        res
+    });
 
-        fn call(&self, req: Request) -> Self::Future {
-             match (req.method(), req.path()) {
-                (&Method::Get, "/") => {
-                    Either::A(futures::future::ok(
-                        Response::new().with_body("Try POSTing data to /echo")
-                    ))
-                },
-                (&Method::Post, "/echo") => {
-                    Either::B(
-                        req.body()
-                            .concat2()
-                            .map(reverse)
-                    )
-                },
-                _ => {
-                    Either::A(futures::future::ok(
-                        Response::new().with_status(StatusCode::NotFound)
-                    ))
-                },
-            }
-        }
-    }
+    let fut1 = pool.spawn_fn(|| {
+        let prime = true;
 
+//        thread::sleep(Duration::from_secs(5));
+        println!("hi from the future2");
+
+
+        let res: Result<bool, ()> = Ok(prime);
+        res
+    });
+
+    let h = handle.spawn_fn(|| {
+        println!("hi from the future1");
+        //        let prime = true;
+        //        let res: Result<bool, bool> = Ok(prime);
+        //        res
+        Ok(())
+    });
+
+    core.run(fut);
+
+
+    //    struct HelloWorld;
+    //    impl Service for HelloWorld {
+    //        // boilerplate hooking up hyper's server types
+    //        type Request = Request;
+    //        type Response = Response;
+    //        type Error = hyper::Error;
+    //        // The future representing the eventual Response your call will
+    //        // resolve to. This can change to whatever Future you need.
+    //        type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+    //
+    //        fn call(&self, _req: Request) -> Self::Future {
+    //            thread::sleep(Duration::from_secs(5));
+    //
+    //            futures::future::ok(
+    //                Response::new()
+    //                    //.with_header(ContentLength(PHRASE.len() as u64))
+    //                    //.with_body(PHRASE)
+    //                    .with_header(ContentLength(prime.to_string().len() as u64))
+    //                    .with_body(prime.to_string())
+    //            )
+    //        }
+    //    }
+    //
+    //
+    //    struct Echo;
+    //    impl Service for Echo {
+    //    type Future = Either<
+    //        FutureResult<Self::Response, Self::Error>,
+    //        Map<Concat2<Body>, fn(Chunk) -> Self::Response>
+    //    >;
+    //    // back to default Response
+    //        type Response = Response;
+    //
+    //        fn call(&self, req: Request) -> Self::Future {
+    //             match (req.method(), req.path()) {
+    //                (&Method::Get, "/") => {
+    //                    Either::A(futures::future::ok(
+    //                        Response::new().with_body("Try POSTing data to /echo")
+    //                    ))
+    //                },
+    //                (&Method::Post, "/echo") => {
+    //                    Either::B(
+    //                        req.body()
+    //                            .concat2()
+    //                            .map(reverse)
+    //                    )
+    //                },
+    //                _ => {
+    //                    Either::A(futures::future::ok(
+    //                        Response::new().with_status(StatusCode::NotFound)
+    //                    ))
+    //                },
+    //            }
+    //        }
+    //    }
 
 
     println!("http://localhost:4000");
-    let addr = "127.0.0.1:4000".parse().unwrap();
-    //let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
-    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
-    server.run().unwrap();
-
+    //    let addr = "127.0.0.1:4000".parse().unwrap();
+    //    let server = Http::new().bind(&addr, || Ok()).unwrap();
+    //    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
+    //    server.run().unwrap();
 }
