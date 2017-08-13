690e1233cafb3702ab98bb77565a5ad4630e8a7e
toidiu <toidiu@protonmail.com>
2017-08-13 16:59:00 -0400

    rename client

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ extern crate futures;
 use futures::Future;
 use tokio_core::reactor::Handle;
 
-mod xml_client;
+mod mta_client;
 mod parse_xml;
 mod file_cache;
 mod parse_html;
@@ -29,7 +29,7 @@ pub fn get_status(handle: &Handle) -> Box<Future<Item=String, Error=hyper::Error
     // use std::time::Duration;
     // thread::sleep(Duration::from_secs(2));
 
-    let result_xml_resp = xml_client::get_mta_status(&handle);
+    let result_xml_resp = mta_client::get_mta_status(&handle);
 
     let result_xml_resp = result_xml_resp.map(|mut xml_resp|{
              let query = parse_xml::parse(&mut xml_resp);
@@ -45,10 +45,10 @@ pub fn get_status(handle: &Handle) -> Box<Future<Item=String, Error=hyper::Error
 
 #[cfg(test)]
 mod tests {
-    use super::xml_client;
+    use super::mta_client;
 
     #[test]
     fn it_fails() {
-        ::xml_client::connect();
+        ::mta_client::connect();
     }
 }
