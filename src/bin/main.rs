0e8842f5dfad8c8616c0cd333f93bc0655e189fa
toidiu <toidiu@protonmail.com>
2017-10-07 15:43:04 -0400

    enable CORS

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,11 @@
-#![deny(warnings)]
+//#![deny(warnings)]
 //#![allow(unused)]
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
 extern crate futures;
-extern crate hyper;
+//extern crate hyper;
+#[macro_use] extern crate hyper;
 extern crate mta_status;
 extern crate tokio_core;
 
@@ -14,6 +15,9 @@ use hyper::{Method, StatusCode};
 use hyper::server::{Http, Service};
 use tokio_core::net::TcpListener;
 use futures::{Future, Stream};
+
+use hyper::header::Headers;
+header! { (AccessControl, "Access-Control-Allow-Origin") => [String] }
 
 #[cfg(debug_assertions)]
 const IS_PROD: bool = false;
@@ -43,12 +47,20 @@ impl Service for GetStatus {
         match (req.method(), req.path()) {
             (&Method::Get, "/") => {
                 let status = mta_status::get_status(&self._handle).map(|stat| {
-                    resp.with_body(stat).with_status(StatusCode::Ok)
+                    let mut headers = Headers::new();
+                    headers.set(AccessControl("*".to_owned()));
+
+                    resp
+                        .with_body(stat)
+                        .with_headers(headers)
+                        .with_status(StatusCode::Ok)
                 });
                 Box::new(status)
             }
             _ => Box::new(futures::future::ok(
-                resp.with_body("no path").with_status(StatusCode::NotFound),
+                resp
+                .with_body("no path")
+                .with_status(StatusCode::NotFound),
             )),
         }
 
